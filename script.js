748b9a2e5b7bfe8a53ef58b1fe23ec3f0e7f623d
Vesclard <fauzanalfansa@gmail.com>
2024-05-17 18:18:25 +0700

    Set computerChoice value depends on randomNumber

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerChoice;
   console.log(randomNumber);
+  if (randomNumber === 1) {
+    computerChoice = "Rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
+  console.log(computerChoice);
 }
 
 getComputerChoice();
